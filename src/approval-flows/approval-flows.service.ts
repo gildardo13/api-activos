23f54318873fb2dcf6d6d7fa26af21e9b5af019f
CICSACORP\grubalcaba <grubalcaba@cicsagruas.net>
2026-06-30 14:37:03 -0600

    fi/comment

--- src/approval-flows/approval-flows.service.ts
+++ src/approval-flows/approval-flows.service.ts
@@ -120,7 +120,7 @@ export class ApprovalFlowsService {
 
   async createSolicitud(request: CreateSolicitudDto, moduleId: string) {
     try {
-      /*const res = await this.workflowApi().post<any>(`approval/request/${moduleId}`, request, {
+      const res = await this.workflowApi().post<any>(`approval/request/${moduleId}`, request, {
         headers: this.buildHeaders(),
       });
       if (res.data === null || res.data === undefined || (Array.isArray(res.data) && res.data.length === 0) || (typeof res.data === "object" && !Array.isArray(res.data) && Object.keys(res.data).length === 0)
@@ -128,9 +128,9 @@ export class ApprovalFlowsService {
         return [];
       }
 
-      return res.data;*/
+      return res.data;
 
-      return [];
+      //return [];
 
 
     } catch (err: any) {
